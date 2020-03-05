d00b8b2e7f8ee3447dd336bf8f2929280956f0fb
Oliver Richter <Oliver.Richter@GFT.com>
2020-03-05 09:21:58 +0100

    Simplify converter usage
    - Implement converters as markup extensions
    - Use typed properties instead of converter parameters

--- CoffeeShop/Views/Converters/BooleanToValueConverter.cs
+++ CoffeeShop/Views/Converters/BooleanToValueConverter.cs
@@ -2,28 +2,21 @@
 using System.Globalization;
 using System.Windows;
 using System.Windows.Data;
+using System.Windows.Markup;
 
 namespace Basta2020Feb.CoffeeShop.Views.Converters
 {
-	internal class BooleanToValueConverter : IValueConverter
+	internal class BooleanToValueConverter : MarkupExtension, IValueConverter
 	{
+		public object IfTrue { get; set; }
+
+		public object IfFalse { get; set; }
+
 		public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
 		{
-			var parameterString = parameter as string;
-			if (parameterString == null)
-			{
-				return DependencyProperty.UnsetValue;
-			}
-
-			var parameters = parameterString.Split(';');
-			if (parameters.Length != 2)
-			{
-				return DependencyProperty.UnsetValue;
-			}
-
 			if (value is bool b)
 			{
-				return b ? parameters[0] : parameters[1];
+				return b ? IfTrue : IfFalse;
 			}
 
 			return DependencyProperty.UnsetValue;
@@ -32,6 +25,11 @@ namespace Basta2020Feb.CoffeeShop.Views.Converters
 		public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
 		{
 			throw new NotImplementedException();
+		}
+
+		public override object ProvideValue(IServiceProvider serviceProvider)
+		{
+			return this;
 		}
 	}
 }
