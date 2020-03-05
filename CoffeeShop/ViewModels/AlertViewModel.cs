f276592847d55f0e14ae15d7ddfb9fa7bb034082
Oliver Richter <Oliver.Richter@GFT.com>
2020-03-05 10:07:38 +0100

    Use binding conventions to cleanup AlertView
    - Binding convention for Text
    - Binding convention for Command
    - Get rid of ICommand overhead
    - CanConfirm Property is also supported

--- CoffeeShop/ViewModels/AlertViewModel.cs
+++ CoffeeShop/ViewModels/AlertViewModel.cs
@@ -1,15 +1,10 @@
 ﻿using System;
-using System.Windows.Input;
 using Basta2020Feb.CoffeeShop.ViewModels.Common;
 
 namespace Basta2020Feb.CoffeeShop.ViewModels
 {
 	internal class AlertViewModel : BaseViewModel, IAlertViewModel
 	{
-		private ICommand _confirmCommand;
-
-		public ICommand ConfirmCommand => _confirmCommand ?? (_confirmCommand = new CommandHelper(Confirm, () => true));
-
 		public string Message { get; }
 
 		public event EventHandler AlertConfirmed;
--- CoffeeShop/ViewModels/IAlertViewModel.cs
+++ CoffeeShop/ViewModels/IAlertViewModel.cs
@@ -1,13 +1,10 @@
 ﻿using System;
-using System.Windows.Input;
 using Basta2020Feb.CoffeeShop.ViewModels.Common;
 
 namespace Basta2020Feb.CoffeeShop.ViewModels
 {
 	internal interface IAlertViewModel : IBaseViewModel
 	{
-		ICommand ConfirmCommand { get; }
-
 		string Message { get; }
 
 		event EventHandler AlertConfirmed;
